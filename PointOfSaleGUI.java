3eb10864c0ef79a33ac2bcf1605e2b58f04da4eb
Jeremy Sanders <sandersjb12@gmail.com>
2015-03-30 22:02:05 -0500

    Added PointOfSaleGUI class and attributes

---
+++ PointOfSaleGUI.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+public class PointOfSaleGUI extends JFrame
+{
+    private JLabel product1Name;
+    private JLabel product1Quantity;
+    private JLabel product1Price;
+
+    private JLabel product2Name;
+    private JLabel product2Quantity;
+    private JLabel product2Price;
+
+    private JLabel product3Name;
+    private JLabel product3Quantity;
+    private JLabel product3Price;
+
+    private JLabel product4Name;
+    private JLabel product4Quantity;
+    private JLabel product4Price;
+
+    private JLabel product5Name;
+    private JLabel product5Quantity;
+    private JLabel product5Price;
+
+    private JLabel saleLabel;
+    private JComboBox saleBox;
+    private JTextField saleField;
+    private JButton recordButton;
+    private String[] products;
+
+    private JLabel revenueLabel;
+    private JLabel revenueNumber;
+
+    private JButton saveButton;
+    private JButton quitButton;
+
+
+}
