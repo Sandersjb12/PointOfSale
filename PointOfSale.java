0536ceb8560b45bbc8da0d51ecd9998def746a19
Jeremy Sanders <sandersjb12@gmail.com>
2015-03-30 21:33:22 -0500

    Added PointOfSale class with main method

---
+++ PointOfSale.java
@@ -0,0 +1,7 @@
+public class PointOfSale
+{
+    public static void main(String[] args)
+    {
+        //record a sale, see list of products, calculate revenue, add product, delete product, save, quit
+    }
+}
