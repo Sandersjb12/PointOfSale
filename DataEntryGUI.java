126b1656ade284517c9be0a7c70d83b14690ed95
Jeremy Sanders <sandersjb12@gmail.com>
2015-03-30 22:56:32 -0500

    Added attributes to DataEntryGUI

--- DataEntryGUI.java
+++ DataEntryGUI.java
@@ -1,6 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class DataEntryGUI extends JFrame
 {
+    int numberOfProducts;
+    Integer[] options={1,2,3,4,5,6};
+    int count;
 
+    private JLabel nameLabel;
+    private JTextField nameField;
+
+    private JLabel priceLabel;
+    private JTextField priceField;
+
+    private JLabel quantityLabel;
+    private JTextField quantityField;
+
+    public DataEntryGUI()
+    {
+        super("Point of Sale v0.01 - Data Entry");
+        setLayout(new GridLayout(3,2));
+        count=0;
+
+        numberOfProducts=(Integer)JOptionPane.showInputDialog(null, "Select number of products:", "Products", JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+    }
 }
--- PointOfSale.java
+++ PointOfSale.java
@@ -1,7 +1,11 @@
+import javax.swing.*;
+
 public class PointOfSale
 {
     public static void main(String[] args)
     {
         //record a sale, see list of products, calculate revenue, add product, delete product, save, quit
+
+        DataEntryGUI d=new DataEntryGUI();
     }
 }
