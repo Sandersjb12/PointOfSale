65b45e2a686f6a0f78618cc3846630aa41ddecbc
Jeremy Sanders <sandersjb12@gmail.com>
2015-04-08 14:44:11 -0500

    finished switch statement in POSGUI

--- PointOfSaleGUI.java
+++ PointOfSaleGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 
 public class PointOfSaleGUI extends JFrame
@@ -44,41 +45,109 @@ public class PointOfSaleGUI extends JFrame
     {
         super("Point of Sale v0.01");
 
+        setLayout(new GridLayout(productList.size()+2,3));
+
         switch(productList.size())
         {
             case 6:
                 product6Name=new JLabel(productList.get(5).getName());
-                product6Price=new JLabel(String.format("$%.2f", productList.get(5).getPrice()));
-                product6Quantity=new JLabel(Integer.toString(productList.get(5).getQuantity()));
+                product6Price=new JLabel(String.format("Price: $%.2f", productList.get(5).getPrice()));
+                product6Quantity=new JLabel(String.format("Quantity: %d", productList.get(5).getQuantity()));
+
             case 5:
                 product5Name=new JLabel(productList.get(4).getName());
-                product5Price=new JLabel(String.format("$%.2f", productList.get(4).getPrice()));
-                product5Quantity=new JLabel(Integer.toString(productList.get(4).getQuantity()));
+                product5Price=new JLabel(String.format("Price: $%.2f", productList.get(4).getPrice()));
+                product5Quantity=new JLabel(String.format("Quantity: %d", productList.get(4).getQuantity()));
+
             case 4:
                 product4Name=new JLabel(productList.get(3).getName());
-                product4Price=new JLabel(String.format("$%.2f", productList.get(3).getPrice()));
-                product4Quantity=new JLabel(Integer.toString(productList.get(3).getQuantity()));
+                product4Price=new JLabel(String.format("Price: $%.2f", productList.get(3).getPrice()));
+                product4Quantity=new JLabel(String.format("Quantity: %d", productList.get(3).getQuantity()));
 
             case 3:
                 product3Name=new JLabel(productList.get(2).getName());
-                product3Price=new JLabel(String.format("$%.2f", productList.get(2).getPrice()));
-                product3Quantity=new JLabel(Integer.toString(productList.get(2).getQuantity()));
+                product3Price=new JLabel(String.format("Price: $%.2f", productList.get(2).getPrice()));
+                product3Quantity=new JLabel(String.format("Quantity: %d", productList.get(2).getQuantity()));
 
             case 2:
                 product2Name=new JLabel(productList.get(1).getName());
-                product2Price=new JLabel(String.format("$%.2f", productList.get(1).getPrice()));
-                product2Quantity=new JLabel(Integer.toString(productList.get(1).getQuantity()));
+                product2Price=new JLabel(String.format("Price: $%.2f", productList.get(1).getPrice()));
+                product2Quantity=new JLabel(String.format("Quantity: %d", productList.get(1).getQuantity()));
 
             case 1:
                 product1Name=new JLabel(productList.get(0).getName());
-                product1Price=new JLabel(String.format("$%.2f", productList.get(0).getPrice()));
-                product1Quantity=new JLabel(Integer.toString(productList.get(0).getQuantity()));
+                product1Price=new JLabel(String.format("Price: $%.2f", productList.get(0).getPrice()));
+                product1Quantity=new JLabel(String.format("Quantity: %d", productList.get(0).getQuantity()));
                 break;
 
             default:
                 throw (new IndexOutOfBoundsException());
         }
 
-        System.out.println(productList);
+        saleLabel=new JLabel("Enter a sale:");
+        saleField=new JTextField();
+
+        products=new String[productList.size()];
+        int index=0;
+        for(Product p:productList)
+        {
+            products[index]=p.getName();
+            index++;
+        }
+        saleBox=new JComboBox<String>(products);
+        recordButton=new JButton("Record Sale");
+
+        revenueLabel=new JLabel("Revenue:");
+        revenueNumber=new JLabel("");
+
+        saveButton=new JButton("Save");
+        quitButton=new JButton("Quit");
+
+        add(product1Name);
+        add(product1Price);
+        add(product1Quantity);
+
+        if (productList.size()>=2)
+        {
+            add(product2Name);
+            add(product2Price);
+            add(product2Quantity);
+        }
+
+        if (productList.size()>=3)
+        {
+            add(product3Name);
+            add(product3Price);
+            add(product3Quantity);
+        }
+
+        if (productList.size()>=4)
+        {
+            add(product4Name);
+            add(product4Price);
+            add(product4Quantity);
+        }
+
+        if (productList.size()>=5)
+        {
+            add(product5Name);
+            add(product5Price);
+            add(product5Quantity);
+        }
+
+        if (productList.size()>=6)
+        {
+            add(product6Name);
+            add(product6Price);
+            add(product6Quantity);
+        }
+
+        add(saleLabel);
+        add(saleBox);
+        add(saleField);
+        add(recordButton);
+
+        add(saveButton);
+        add(quitButton);
     }
 }
